03839ce422479710b655bc7bb05a65fc445fff26
Christoph M. Becker <cmbecker69@gmx.de>
2015-06-01 23:43:34 +0200

    fixed CS violations

--- admin.php
+++ admin.php
@@ -1,55 +1,75 @@
 <?php
 
-
 /**
  * Back-End of Tablesorter_XH.
  *
- * Copyright (c) 2012-2015 Christoph M. Becker (see license.txt)
+ * PHP version 4 and 5
+ *
+ * @category  CMSimple_XH
+ * @package   Tablesorter
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2015 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link      http://3-magi.net/?CMSimple_XH/Tablesorter_XH
  */
-
 
 if (!defined('CMSIMPLE_XH_VERSION')) {
     header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
-
 /**
  * Returns the version information view.
  *
- * @return string  The (X)HTML.
+ * @return string (X)HTML
+ *
+ * @global array The paths of system files and folders.
  */
-function Tablesorter_version() {
+function Tablesorter_version()
+{
     global $pth;
 
-    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Tablesorter_XH">Tablesorter_XH</a></h1>'
-        . tag('img style="float:left; margin-right:10px" src="' . $pth['folder']['plugins']
-              . 'tablesorter/tablesorter.png" alt="Plugin icon"')
+    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Tablesorter_XH">'
+        . 'Tablesorter_XH</a></h1>'
+        . tag(
+            'img style="float:left; margin-right:10px" src="'
+            . $pth['folder']['plugins']
+            . 'tablesorter/tablesorter.png" alt="Plugin icon"'
+        )
         . '<p style="margin-top: 1em">Version: ' . TABLESORTER_VERSION . '</p>'
-        . '<p>Copyright &copy; 2012-2015 <a href="http://3-magi.net/">Christoph M. Becker</a></p>'
-	. '<p>Tablesorter_XH is powered by the <a href="https://github.com/Mottie/tablesorter">'
-	. 'jQuery tablesorter plugin</a>.</p>'
-        . '<p style="text-align: justify">This program is free software: you can redistribute it and/or modify'
+        . '<p>Copyright &copy; 2012-2015 <a href="http://3-magi.net/">'
+        . 'Christoph M. Becker</a></p>'
+        . '<p>Tablesorter_XH is powered by the <a href="https://github.com/Mottie/'
+        . 'tablesorter">jQuery tablesorter plugin</a>.</p>'
+        . '<p style="text-align: justify">'
+        . 'This program is free software: you can redistribute it and/or modify'
         . ' it under the terms of the GNU General Public License as published by'
         . ' the Free Software Foundation, either version 3 of the License, or'
         . ' (at your option) any later version.</p>'
-        . '<p style="text-align: justify">This program is distributed in the hope that it will be useful,'
+        . '<p style="text-align: justify">'
+        . 'This program is distributed in the hope that it will be useful,'
         . ' but WITHOUT ANY WARRANTY; without even the implied warranty of'
         . ' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
         . ' GNU General Public License for more details.</p>'
-        . '<p style="text-align: justify">You should have received a copy of the GNU General Public License'
+        . '<p style="text-align: justify">'
+        . 'You should have received a copy of the GNU General Public License'
         . ' along with this program.  If not, see'
-        . ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>';
+        . ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/'
+        . '</a>.</p>';
 }
 
 
 /**
  * Returns the requirements information view.
  *
- * @return string  The (X)HTML.
+ * @return string (X)HTML
+ *
+ * @global array The paths of system files and folders.
+ * @global array The localization of the core.
+ * @global array The localization of the plugins.
  */
 function Tablesorter_systemCheck()
-{ // RELEASE-TODO
+{
     global $pth, $tx, $plugin_tx;
 
     define('TABLESORTER_PHP_VERSION', '4.0.7');
@@ -58,30 +78,28 @@ function Tablesorter_systemCheck()
     $ok = tag('img src="' . $imgdir . 'ok.png" alt="ok"');
     $warn = tag('img src="' . $imgdir . 'warn.png" alt="warning"');
     $fail = tag('img src="' . $imgdir . 'fail.png" alt="failure"');
-    $o = tag('hr').'<h4>' . $ptx['syscheck_title'] . '</h4>'
-	. (version_compare(PHP_VERSION, TABLESORTER_PHP_VERSION) >= 0 ? $ok : $fail)
-        . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_phpversion'], TABLESORTER_PHP_VERSION)
+    $o = tag('hr') . '<h4>' . $ptx['syscheck_title'] . '</h4>'
+        . (version_compare(PHP_VERSION, TABLESORTER_PHP_VERSION) >= 0 ? $ok : $fail)
+        . '&nbsp;&nbsp;'
+        . sprintf($ptx['syscheck_phpversion'], TABLESORTER_PHP_VERSION)
         . tag('br') . tag('br');
-//    foreach (array() as $ext) {
-//	$o .= (extension_loaded($ext) ? $ok : $fail)
-//	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_extension'], $ext) . tag('br');
-//    }
-    $o .= /*tag('br') . */(strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
-	. '&nbsp;&nbsp;' . $ptx['syscheck_encoding'] . tag('br');
+    $o .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
+        . '&nbsp;&nbsp;' . $ptx['syscheck_encoding'] . tag('br');
     $o .= (!get_magic_quotes_runtime() ? $ok : $fail)
-	. '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes'] . tag('br');
-    $o .= (file_exists($pth['folder']['plugins'].'jquery/jquery.inc.php') ? $ok : $fail)
-	. '&nbsp;&nbsp;' . $ptx['syscheck_jquery'] . tag('br') . tag('br');
+        . '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes'] . tag('br');
+    $filename = $pth['folder']['plugins'] . 'jquery/jquery.inc.php';
+    $o .= (file_exists($filename) ? $ok : $fail)
+        . '&nbsp;&nbsp;' . $ptx['syscheck_jquery'] . tag('br') . tag('br');
     foreach (array('config/', 'languages/') as $folder) {
-	$folders[] = $pth['folder']['plugins'] . 'tablesorter/' . $folder;
+        $folders[] = $pth['folder']['plugins'] . 'tablesorter/' . $folder;
     }
     foreach ($folders as $folder) {
-	$o .= (is_writable($folder) ? $ok : $warn)
-	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder) . tag('br');
+        $o .= (is_writable($folder) ? $ok : $warn)
+            . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder)
+            . tag('br');
     }
     return $o;
 }
-
 
 /*
  * Handle the plugin administration
--- index.php
+++ index.php
@@ -3,26 +3,38 @@
 /**
  * Front-end of Tablesorter_XH.
  *
- * Copyright (c) 2012-2015 Christoph M. Becker (see license.txt)
+ * PHP version 4 and 5
+ *
+ * @category  CMSimple_XH
+ * @package   Tablesorter
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2015 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link      http://3-magi.net/?CMSimple_XH/Tablesorter_XH
  */
-
 
 if (!defined('CMSIMPLE_XH_VERSION')) {
     header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
-
+/**
+ * The plugin version.
+ */
 define('TABLESORTER_VERSION', '1beta1');
-
 
 /**
  * Makes all <table class="sortable"> sortable.
  *
- * @access public
  * @return void
+ *
+ * @global string The (X)HTML fragment to insert into the head element.
+ * @global array  The paths of system files and folders.
+ * @global array  The configuration of the plugins.
+ *
+ * @staticvar bool $again Whether the function has already been executed.
  */
-function Tablesorter()
+function tablesorter()
 {
     global $hjs, $pth, $plugin_cf;
     static $again = false;
@@ -33,23 +45,27 @@ function Tablesorter()
     $again = true;
     $pcf = $plugin_cf['tablesorter'];
 
-    require_once $pth['folder']['plugins'] . 'jquery/jquery.inc.php';
+    include_once $pth['folder']['plugins'] . 'jquery/jquery.inc.php';
     include_jQuery();
-    include_jQueryPlugin('tablesorter', $pth['folder']['plugins']
-                         . 'tablesorter/tablesorter/js/jquery.tablesorter.js');
-    $theme = is_readable("{$pth['folder']['plugins']}tablesorter/tablesorter/css/theme.$pcf[theme].css")
-        ? $pcf['theme']
-        : 'default';
-    $css = $theme == 'default'
-        ? "{$pth['folder']['plugins']}tablesorter/tablesorter/css/theme.default.css"
-        : "{$pth['folder']['plugins']}tablesorter/tablesorter/css/theme.$pcf[theme].css";
+    include_jQueryPlugin(
+        'tablesorter',
+        $pth['folder']['plugins']
+        . 'tablesorter/tablesorter/js/jquery.tablesorter.js'
+    );
+    $theme = $pcf['theme'];
+    $filename = $pth['folder']['plugins'] . 'tablesorter/tablesorter/css/theme.'
+        . $pcf['theme'] . '.css';
+    if (!is_readable($filename)) {
+        $theme = 'default';
+        $filename = $pth['folder']['plugins']
+            . 'tablesorter/tablesorter/css/theme.default.css';
+    }
     $widgets = $pcf['zebra'] ? ', widgets: ["zebra"]' : '';
-    $hjs .= tag('link rel="stylesheet" href="' . $css . '" type="text/css"')
+    $hjs .= tag('link rel="stylesheet" href="' . $filename . '" type="text/css"')
         . '<script type="text/javascript">/* <![CDATA[ */jQuery(function()'
         . ' {jQuery("table.sortable").tablesorter({theme: "' . $theme
         . '", sortLocaleCompare: true' . $widgets . '})})/* ]]> */</script>';
 }
-
 
 /*
  * Handle auto mode.
